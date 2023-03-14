83446d7e6dbbb78b26b1f1c30bc3df9733b1c25d
Nick Bayern <jeldak@icloud.com>
2023-03-14 15:00:18 +0200

    avatar

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+// const multer = require('multer');
+// const path = require('path');
+// const fs = require('fs/promises');
+require('dotenv').config();
 
+const authRouter = require("./routes/api/auth")
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
@@ -10,8 +15,10 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
+app.use(express.static("public"))
 app.use(express.json())
 
+app.use("/api/users", authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
@@ -19,7 +26,8 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message, })
 })
 
 module.exports = app
